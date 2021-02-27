22371e4db683b6b1794d5c64bd56090156a9ebb6
vr26 <78889190+vr26@users.noreply.github.com>
2021-02-27 16:50:23 +0530

    Add files via upload

---
+++ AddTwoIntegers.java
@@ -0,0 +1,18 @@
+public class AddTwoIntegers 
+{
+
+    public static void main(String[] args) 
+    {
+        
+        /* Declaring 2 Integer values */
+        int first = 10;
+        int second = 20;
+
+        System.out.println("Enter two numbers: " + first + " " + second);
+        
+        /* Addition Process */
+        int sum = first + second;
+        /* Displaying */
+        System.out.println("The sum is: " + sum);
+    }
+}
